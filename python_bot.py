c9ab20c42f1f4868adaaf5ded5aef0b7768cb302
Oleg <sagdeev.ol@yandex.ru>
2024-09-17 00:43:51 +0300

    Update python_bot.py

--- python_bot.py
+++ python_bot.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 
-api = '7024624175:AAE8PSvSSv3sUffMSDKWyFXK7dTVhUkXlRc'
+api = 'ключ'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
